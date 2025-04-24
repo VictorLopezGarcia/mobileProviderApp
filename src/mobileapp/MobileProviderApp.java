f151c60564bbf444753f53b9e1f99aa3a168fc27
VictorLopezGarcia <victlopgar@gmail.com>
2025-04-24 21:06:59 +0200

    Initial commit

---
+++ src/mobileapp/MobileProviderApp.java
@@ -0,0 +1,21 @@
+package mobileapp;
+
+
+
+
+/** Create by
+ * @author Ian Roca
+ * @author Leo Ces
+ * @author Enmanuel Lledo
+ */
+public class MobileProviderApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola");
+    
+    }
+}
